aef69a250878b117389447f4aa3e74ba0ab55866
aornugent <aornugent@gmail.com>
2026-03-18 21:08:04 +1100

    Remove unused file

--- inst/examples/leaf_thermal/src/leaf_thermal_pars.hpp
+++
@@ -1,9 +0,0 @@
-// Define a struct to hold parameters for the leaf thermal model
-struct LeafThermalPars
-{
-  // Leaf thermal behaviour
-  double k_H = 0.5;       // 1/h
-  double g_tr_max = 1.0;  // °C/h cooling
-  double m_tr = 0.5;      // steepness
-  double T_tr_mid = 30.0; // °C
-};
